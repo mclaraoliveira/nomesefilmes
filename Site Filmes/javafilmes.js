5a687c6a4b26c197c30f96ee2dbdab0e00bbc7c5
Clara Oliveira <122572905+mclaraoliveira@users.noreply.github.com>
2023-09-09 20:29:51 -0300

    Add files via upload

---
+++ Site Filmes/javafilmes.js
@@ -0,0 +1,557 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const amanhecerDiv = document.getElementById('amanhecer');
+
+    if (amanhecerDiv) {
+        amanhecerDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=PjFLL3nLP3w&ab_channel=CliqueTrailers', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const invisivelDiv = document.getElementById('invisivel');
+
+    if (invisivelDiv) {
+        invisivelDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=mgzV9aSS2y0&ab_channel=VEJAS%C3%83OPAULO', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const chegadaDiv = document.getElementById('chegada');
+
+    if (chegadaDiv) {
+        chegadaDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=rNciXGzYZms&ab_channel=SonyPicturesBrasil', '_blank');
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const origemDiv = document.getElementById('origem');
+
+    if (origemDiv) {
+        origemDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=HiixbtN-O24&ab_channel=WarnerBros.PicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const aquariusDiv = document.getElementById('aquarius');
+
+    if (aquariusDiv) {
+        aquariusDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=VB-5rodvHUc&ab_channel=VitrineFilmes', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const adoraveisDiv = document.getElementById('adoraveis');
+
+    if (adoraveisDiv) {
+        adoraveisDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=7nc1GE_hnLs&ab_channel=SonyPicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const bladeDiv = document.getElementById('blade');
+
+    if (bladeDiv) {
+        bladeDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=okJdrvOdZFI&ab_channel=F%C3%A3sdeCinema', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const bonequinhaDiv = document.getElementById('bonequinha');
+
+    if (bonequinhaDiv) {
+        bonequinhaDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=mTSJM-PPmDs&ab_channel=OldflixBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const brooklynDiv = document.getElementById('brooklyn');
+
+    if (brooklynDiv) {
+        brooklynDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=ZEhuC6rBsE4&ab_channel=ParisFilmes', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const bastardosDiv = document.getElementById('bastardos');
+
+    if (bastardosDiv) {
+        bastardosDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=w1O5LThfp7w&ab_channel=trailersbygetro2', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const comportamentoDiv = document.getElementById('comportamento');
+
+    if (comportamentoDiv) {
+        comportamentoDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=51eSgsmeCUU&ab_channel=F%C3%A3sdeCinema', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const bacurauDiv = document.getElementById('bacurau');
+
+    if (bacurauDiv) {
+        bacurauDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=qlheaoLnewE&ab_channel=F%C3%A3sdeCinema', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cidadeDiv = document.getElementById('cidade');
+
+    if (cidadeDiv) {
+        cidadeDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=fZJUKixyeXM&ab_channel=O2Filmes', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const perderDiv = document.getElementById('perder');
+
+    if (perderDiv) {
+        perderDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=3IDeifYOCvM&ab_channel=RepassandoOficial', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const casablancaDiv = document.getElementById('casablanca');
+
+    if (casablancaDiv) {
+        casablancaDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=VWmEkGkBW4w&ab_channel=OldflixBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const casamentoDiv = document.getElementById('casamento');
+
+    if (casamentoDiv) {
+        casamentoDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=OFG-a7fx0vI&ab_channel=AbraCadaver', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const corraDiv = document.getElementById('corra');
+
+    if (corraDiv) {
+        corraDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=mDGV5UucTu8&ab_channel=UniversalPicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cisneDiv = document.getElementById('cisne');
+
+    if (cisneDiv) {
+        cisneDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=t-weIgpXS6g', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const magiaDiv = document.getElementById('magia');
+
+    if (magiaDiv) {
+        magiaDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=R7uixLkpjPs&ab_channel=WarnerBros.', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const futuroDiv = document.getElementById('futuro');
+
+    if (futuroDiv) {
+        futuroDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=R894dvwNo4w&ab_channel=HeroXCast-AnimeeCulturaNerd', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const repenteDiv = document.getElementById('repente');
+
+    if (repenteDiv) {
+        repenteDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=NV7PBRBcxu0&ab_channel=CliqueTrailers', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const desejoDiv = document.getElementById('desejo');
+
+    if (desejoDiv) {
+        desejoDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=fiv8Gy5CJ0Y&ab_channel=RottenTomatoesClassicTrailers', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const dirtyDiv = document.getElementById('dirty');
+
+    if (dirtyDiv) {
+        dirtyDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=U1tw7CJHYJQ&ab_channel=ArquivodeTrailers', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const donnieDiv = document.getElementById('donnie');
+
+    if (donnieDiv) {
+        donnieDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=rV817qNlR3g&ab_channel=S%C3%B3Trailers', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const facasDiv = document.getElementById('facas');
+
+    if (facasDiv) {
+        facasDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=w8GBFvqP1cM&ab_channel=F%C3%A3sdeCinema', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const emmaDiv = document.getElementById('emma');
+
+    if (emmaDiv) {
+        emmaDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=1kxiY0Nfl34&ab_channel=F%C3%A3sdeCinema', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const elaDiv = document.getElementById('ela');
+
+    if (elaDiv) {
+        elaDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=TggD91pV6KE&ab_channel=SonyPicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const elvisDiv = document.getElementById('elvis');
+
+    if (elvisDiv) {
+        elvisDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=YP5D6vTRlsU&ab_channel=WarnerBros.PicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const caraDiv = document.getElementById('cara');
+
+    if (caraDiv) {
+        caraDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=lDe5oho-SVE&ab_channel=Player2Tube', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const ritmoDiv = document.getElementById('ritmo');
+
+    if (ritmoDiv) {
+        ritmoDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=TJrKYWPBTrc&ab_channel=SonyPicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const fragmentadoDiv = document.getElementById('fragmentado');
+
+    if (fragmentadoDiv) {
+        fragmentadoDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=7l4SzfMstLE&ab_channel=UniversalPicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const certaDiv = document.getElementById('certa');
+
+    if (certaDiv) {
+        certaDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=yVAD4fYRcvA&ab_channel=thecoolidge', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const footlooseDiv = document.getElementById('footloose');
+
+    if (footlooseDiv) {
+        footlooseDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=qCy3AmBMTpQ&ab_channel=thecoolidge', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const forrestDiv = document.getElementById('forrest');
+
+    if (forrestDiv) {
+        forrestDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=vDY_uZAaU7g&ab_channel=RepassandoOficial', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const francesDiv = document.getElementById('frances');
+
+    if (francesDiv) {
+        francesDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=4EmU4vSL8jQ&ab_channel=VitrineFilmes', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const freshDiv = document.getElementById('fresh');
+
+    if (freshDiv) {
+        freshDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=TGvMPnyX6Bs&ab_channel=Star%2BBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const exemplarDiv = document.getElementById('exemplar');
+
+    if (exemplarDiv) {
+        exemplarDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=tKwEdRC26xA&ab_channel=20thCenturyStudiosBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const genioDiv = document.getElementById('genio');
+
+    if (genioDiv) {
+        genioDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=ReIJ1lbL-Q8&ab_channel=Miramax', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const ghostDiv = document.getElementById('ghost');
+
+    if (ghostDiv) {
+        ghostDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=jDVBHSe6W0U&ab_channel=MacPhoenix82', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const greaseDiv = document.getElementById('grease');
+
+    if (greaseDiv) {
+        greaseDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=FG93ty5MBZQ&ab_channel=Grease%281978%29', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const gravidadeDiv = document.getElementById('gravidade');
+
+    if (gravidadeDiv) {
+        gravidadeDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=RgevUaS-7nI&ab_channel=WarnerBros.PicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const interrompidaDiv = document.getElementById('interrompida');
+
+    if (interrompidaDiv) {
+        interrompidaDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=9mt3ZDfg6-w&ab_channel=Portr%C3%A1sdaslinhas', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const potterDiv = document.getElementById('potter');
+
+    if (potterDiv) {
+        potterDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=O7JVilOQdZ4&ab_channel=RepassandoOficial', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const aranhaDiv = document.getElementById('aranha');
+
+    if (aranhaDiv) {
+        aranhaDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=_4is7I_ZxTg&ab_channel=MarvelBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sallyDiv = document.getElementById('sally');
+
+    if (sallyDiv) {
+        sallyDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=imf92LMQKJQ&ab_channel=RepassandoOficial', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sozinhoDiv = document.getElementById('sozinho');
+
+    if (sozinhoDiv) {
+        sozinhoDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=lpHKXyko358&ab_channel=HojeEuQueroVoltarSozinho', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hushDiv = document.getElementById('hush');
+
+    if (hushDiv) {
+        hushDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=Q_P8WCbhC6s&ab_channel=RottenTomatoesIndie', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const halloweenDiv = document.getElementById('halloween');
+
+    if (halloweenDiv) {
+        halloweenDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=emN1KbD_DxM&ab_channel=RepassandoOficial', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const halloweenDiv = document.getElementById('halloween');
+
+    if (halloweenDiv) {
+        halloweenDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=emN1KbD_DxM&ab_channel=RepassandoOficial', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const itDiv = document.getElementById('it');
+
+    if (itDiv) {
+        itDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=UllUiLEXB_g&ab_channel=WarnerBros.PicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const interestelarDiv = document.getElementById('interestelar');
+
+    if (interestelarDiv) {
+        interestelarDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=BYUZhddDbdc&ab_channel=WarnerBros.PicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const ilhaDiv = document.getElementById('ilha');
+
+    if (ilhaDiv) {
+        ilhaDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=gL4rP2prdfg&ab_channel=FilmesdoChico', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const infiltradoDiv = document.getElementById('infiltrado');
+
+    if (infiltradoDiv) {
+        infiltradoDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=bbOJwWSEUmo&ab_channel=UniversalPicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const invocacaoDiv = document.getElementById('invocacao');
+
+    if (invocacaoDiv) {
+        invocacaoDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=GQrrXceHn2E&ab_channel=WarnerBros.PicturesBrasil', '_blank'); 
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const irmaosDiv = document.getElementById('irmaos');
+
+    if (irmaosDiv) {
+        irmaosDiv.addEventListener('click', function() {
+            window.open('https://www.youtube.com/watch?v=DhfAT4jrdQg&ab_channel=F%C3%A3sdeCinema', '_blank'); 
+        });
+    }
+});
+
+
+
+
+
+
+
+

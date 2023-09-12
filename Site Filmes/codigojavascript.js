917cdfb035c761ac1f680d9a89096c288f485291
Clara Oliveira <122572905+mclaraoliveira@users.noreply.github.com>
2023-09-12 09:41:57 -0300

    poucas mudanças

--- Site Filmes/codigojavascript.js
+++ Site Filmes/codigojavascript.js
@@ -1,11 +1,13 @@
-let nome = document.getElementById('nome');
+const inputElement = document.getElementById("nome");
+const buttonElement = document.getElementById("botao");
+const nome = inputElement.value;
+const inicial = nome[0];
 
-function identificarLetra() {
-    switch (nome[0]) {
-        case 'A':
-            
-    }
-
-}
-
-
+buttonElement.addEventListener("click", function () {
+  if (inicial == A) {
+    window.open(
+      "https://www.youtube.com/watch?v=DhfAT4jrdQg&ab_channel=F%C3%A3sdeCinema",
+      "_blank"
+    );
+  }
+});
